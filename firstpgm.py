67435d7515e289af3b6cb5f3a9d9ee21a53ad2bd
U2NG <rotuji@ucsd.edu>
2016-10-22 20:40:56 -0700

    first commit from rpi

---
+++ firstpgm.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+import sys
+def prompt():
+	response = sys.stdin.readline().strip()
+	return response
+fields = ["Please tell me your name: ", "What schoold do you go to?: ", "What year are you in?: "]
+answers = []
+for field in fields:
+	print (field),
+	v = prompt()
+	answers += [v]
+
+print("""Hello %s! You go to %s and you are in year %s""" % (answers[0], answers[1], answers[2]))
+
+year = int(answers[2])
+
+if year == 9:
+	print("Hopr your GCSE\'s are going well.")
+elif year == 8:
+	print("I hope you are studying for your exams!")
+else:
+	print("Thanks for playing")
+
+
+
