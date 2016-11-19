e47211daa3e8ac1a3efd3318b89484deb5525586
U2NG <rotsuji@ucsd.edu>
2016-11-19 09:29:04 -0800

    finished buzzer code

--- 6-morsecode.py
+++ 6-morsecode.py
@@ -24,3 +24,45 @@ def dot(): # a single morse dot
     time.sleep(0.1)
 
 def dash(): # a single morse dash
+    GPIO.output(PINBuzzer, GPIO.HIGH)
+    time.sleep(0.3)
+    GPIO.output(PINBuzzer, GPIO.LOW)
+    time.sleep(0.1)
+
+def letterSpace(): #The spave between letters
+    time.sleep(0.2)
+
+def wordSpace(): # the space between words
+    time.sleep(0.6)
+
+def morseS(): # the morese for S, ...
+    dot()
+    dot()
+    dot()
+
+def morseO(): # the morese for o, ---
+    dash()
+    dash()
+    dash()
+
+os.system('clear') # Clears the screen
+
+print("Morese Code")
+
+#prompt
+loop_count = input("How many times would you like SOS to loop?: ")
+loop_count = int(loop_count) # Convert text input into an integer
+
+while loop-count > 0: # loop around the chosen number of times
+    loop_count = loop_count - 1
+    morseS()
+    letterSpace()
+    morseO()
+    letterSpace()
+    morseS()
+    wordSpace()
+
+GPIO.cleanup()
+
+
+    
