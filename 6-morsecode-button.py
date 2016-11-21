16db0e608a217bb6945f914db7e4d8b79d41d783
U2NG <rotsuji@ucsd.edu>
2016-11-20 23:02:43 -0800

    adding program for button to control morse code

--- 6-morsecode-button.py
+++ 6-morsecode-button.py
@@ -8,9 +8,14 @@ import time
 import RPi.GPIO as GPIO
 
 # Set the GPIO pin naming mode
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-PINBuzzer = 26 # sets teh buzzer pin 25
+GPIO.setmode(GPIO.BCM) #set the GPIO pin naming mode
+GPIO.setwarnings(False) # supress warnings
+PINBuzzer = 26 # sets the buzzer pin 26
+
+
+ButtonPin = 25 # Set pin 25 as an input pin
+GPIO.setup(ButtonPin, GPIO.IN) 
+
 
 # Sets PINBuzzer as an output pin and initialize it to 'off'
 GPIO.setup(PINBuzzer, GPIO.OUT)
@@ -29,40 +34,63 @@ def dash(): # a single morse dash
     GPIO.output(PINBuzzer, GPIO.LOW)
     time.sleep(0.1)
 
-def letterSpace(): #The spave between letters
+def letterSpace(): #The space between letters
     time.sleep(0.2)
 
 def wordSpace(): # the space between words
     time.sleep(0.6)
 
-def morseS(): # the morese for S, ...
+def morseS(): # the morse for S, ...
     dot()
     dot()
     dot()
 
-def morseO(): # the morese for o, ---
+def morseO(): # the morse for o, ---
     dash()
     dash()
     dash()
 
 os.system('clear') # Clears the screen
 
-print("Morese Code")
+# print("Morse Code")
+print("------------------")
+print( "Button + GPIO + Morse code")
+print("------------------")
+print(GPIO.input(ButtonPin))
+
+# If the button is pressed, ButtonPin will be FALSE
+while True:
+	# If the button is pressed, ButtonPin will be 'false'
+    if GPIO.input(ButtonPin) == False:
+            print("Sending SOS")
+            print(GPIO.input(ButtonPin))
+            morseS()
+            letterSpace()
+            morseO()
+            letterSpace()
+            morseS()
+            wordSpace()
+            time.sleep(2) # sleep for 2 seconds
+    else:
+	    os.system('clear') # Clears the screen
+	    print("Waiting for you to send an SOS")
+    time.sleep(0.5) # Sleep for 0.5 seconds
+
 
 #prompt
-loop_count = input("How many times would you like SOS to loop?: ")
-loop_count = int(loop_count) # Convert text input into an integer
+#loop_count = input("How many times would you like SOS to loop?: ")
+#loop_count = int(loop_count) # Convert text input into an integer
 
-while loop_count > 0: # loop around the chosen number of times
-    loop_count = loop_count - 1
-    morseS()
-    letterSpace()
-    morseO()
-    letterSpace()
-    morseS()
-    wordSpace()
+#while loop_count > 0: # loop around the chosen number of times
+#    loop_count = loop_count - 1
+#    morseS()
+#    letterSpace()
+#    morseO()
+#    letterSpace()
+#    morseS()
+#    wordSpace()
 
-GPIO.cleanup()
+#GPIO.cleanup()
 
 
     
